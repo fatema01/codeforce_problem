a9ca7e424d27101ba9430845d14e6683df6d8adc
fatema01 <60727304+fatema01@users.noreply.github.com>
2020-03-05 17:14:17 +0600

    Create codeforce339A.cpp

---
+++ codeforce339A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int l,i,j;
+    char s[100];
+    cin>>s;
+    l=strlen(s);
+    for(i=0;i<l;i+=2){
+        for(j=0;j<l-i-2;j+=2){
+            if(s[j]>s[j+2]){
+                swap(s[j],s[j+2]);
+            }
+        }
+    }
+    cout<<s<<endl;
+    return 0;
+}
